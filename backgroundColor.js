8ec4143d08b989d5894d62e41dcaef2d775a4fa2
Olabode1232 <olukanstephen.olabode@gmail.com>
2022-03-15 15:20:18 +0100

    second commit

---
+++ backgroundColor.js
@@ -0,0 +1,6 @@
+let colors = ["yellow","green","blue","black"];
+
+const color = document.querySelector('.color');
+const btn = document.querySelector('.btn');
+let randomNumber = 2
+btn.addEventListener('click', () => console.log(document.body) )
